58cbd6ff4d8cb68da728752baa2b0f261ad4c091
mariog44 <mg441236@gmail.com>
2024-09-02 15:07:33 -0500

    AJUSTE DE RUTAS. Se crearon nuevas rutas para dirigirse a las vistas creadas anteriormente.

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import SkillsView from '@/views/SkillsView.vue'
+import TechnologiesView from '@/views/TechnologiesView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
+import LoginView from '@/views/LoginView.vue'
+import HomeLoggedView from '@/views/HomeLoggedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/About',
+      name: 'About',
+      component: AboutView
+    },
+    {
+      path: '/Skills',
+      name: 'Skills',
+      component: SkillsView
+    },
+    {
+      path: '/Technologies',
+      name: 'Technologies',
+      component: TechnologiesView
+    },
+    {
+      path: '/Projects',
+      name: 'Projects',
+      component: ProjectsView
+    },
+    {
+      path: '/LogIn',
+      name: 'LogIn',
+      component: LoginView
+    },
+    {
+      path: '/HomeLogged',
+      name: 'HomeLogged',
+      component: HomeLoggedView
     }
   ]
 })
