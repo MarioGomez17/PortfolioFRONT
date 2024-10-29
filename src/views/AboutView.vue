0853c003a29908328380e0dcb619b71bc9b642d2
mariogomez441236 <mariogomez441236@gmail.com>
2024-10-29 13:32:44 -0500

    ADICION DE VISTAS. Se agrego una nueva vista (EDUCATION) y se modificó la interfaz de las otras para mostrar su información correspondiente.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="AboutContent">
+    <div class="InformationGroup">
+      <AboutComponent Title="Nombre" :Contetn="Name_Developer"></AboutComponent>
+      <AboutComponent Title="City" :Contetn="City_Developer"></AboutComponent>
+    </div>
+    <div class="InformationGroup">
+      <AboutComponent Title="Email" :Contetn="Email_Developer"></AboutComponent>
+      <LinkComponent Title="LinkedIn" :Contetn="LinkedinName_Developer" :Url="LinkedInUrl_Developer"></LinkComponent>
+      <LinkComponent Title="GitHub" :Contetn="GitHubName_Developer" :Url="GitHubUrl_Developer"></LinkComponent>
+    </div>
+    <div class="InformationGroup">
+      <EducationsComponet Title="Education" :Elements="Education_Developer"></EducationsComponet>
+      <LanguagesComponent Title="Languages" :Elements="Languages_Developer"></LanguagesComponent>
+      <SkillsComponent Title="Skills" :Elements="Skills_Developer"></SkillsComponent>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import AboutComponent from '@/components/AboutComponents/InformationComponent.vue';
+import LinkComponent from '@/components/AboutComponents/LinkComponent.vue';
+import SkillsComponent from '@/components/AboutComponents/SkillsComponent.vue';
+import LanguagesComponent from '@/components/AboutComponents/LanguagesComponent.vue';
+import EducationsComponet from '@/components/AboutComponents/EducationsComponet.vue';
+import GetOneDeveloper from '../Services/DeveloperServices/GetOneDeveloper'
+
+var Developer = null;
+var Name_Developer = ref('')
+var Email_Developer = ref('')
+var LinkedinName_Developer = ref('')
+var LinkedInUrl_Developer = ref('')
+var GitHubName_Developer = ref('')
+var GitHubUrl_Developer = ref('')
+var City_Developer = ref('')
+var Skills_Developer = ref([])
+var Languages_Developer = ref([])
+var Education_Developer = ref([])
+
+onMounted(async () => {
+  Developer = await GetOneDeveloper(1);
+  Name_Developer.value += Developer.Name_Developer
+  Name_Developer.value += ' '
+  Name_Developer.value += Developer.LastName_Developer
+  Email_Developer.value = Developer.Email_Developer
+  LinkedinName_Developer.value = Developer.LinkedinName_Developer
+  LinkedInUrl_Developer.value = Developer.LinkedinUrl_Developer
+  GitHubName_Developer.value = Developer.GitHubName_Developer
+  GitHubUrl_Developer.value = Developer.GitHubUrl_Developer
+  City_Developer.value = Developer.City_Developer.Name_City;
+  City_Developer.value += ', ';
+  City_Developer.value += Developer.City_Developer.Department_City.Name_Department;
+  City_Developer.value += ', ';
+  City_Developer.value += Developer.City_Developer.Department_City.Country_Department.Name_Country;
+  Skills_Developer.value = Developer.Skills_Developer
+  Languages_Developer.value = Developer.Languages_Developer
+  Education_Developer.value = Developer.Education_Developer
+})
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -11,5 +67,21 @@
     display: flex;
     align-items: center;
   }
+}
+
+.AboutContent {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.InformationGroup {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: flex-start;
 }
 </style>
--- src/views/TechnologiesView.vue
+++ src/views/TechnologiesView.vue
@@ -1,12 +1,78 @@
 <script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+import LogoComponent from '@/components/TechnologyComponents/LogoComponent.vue';
+import GetOneDeveloper from '@/Services/DeveloperServices/GetOneDeveloper';
+import SortTechnologies from '@/Services/TechnologyServices/SortTechnologies';
+
+var Developer = null;
+const SkillsTechnologies = ref([]);;
+
+onMounted(async () => {
+    Developer = await GetOneDeveloper(1)
+    SkillsTechnologies.value = SortTechnologies(Developer.Technologies_Developer)
+})
+
 </script>
 
 <template>
-    <main>
-        <div class="MainContent">
-            <h1>Hello World!</h1>
+    <main class="MainContent">
+        <div class="DivMainContent">
+            <div v-for="(Skill, Key) in SkillsTechnologies" :key="Skill.value" class="DivDivison">
+                <h3 class="TitleDivison">{{ Key }}</h3>
+                <div class="DivTechnologies">
+                    <LogoComponent v-for="Technology in Skill" :key="Technology.Id_Technology"
+                        :ImageRoute="Technology.Logo_Technology" :ImageAlt="Technology.Name_Technology" />
+                </div>
+            </div>
         </div>
     </main>
 </template>
-<style scoped></style>
+
+<style scoped>
+@import '../assets/base.css';
+
+.MainContent {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+
+}
+
+.DivMainContent {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 15px;
+    overflow: auto;
+    scrollbar-width: none;
+}
+
+.DivDivison {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;
+    gap: 15px;
+}
+
+.DivTechnologies {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 15px;
+}
+
+.TitleDivison {
+    font-family: var(--Font);
+    text-transform: uppercase;
+    color: var(--TextColor);
+    font-size: 40px;
+    width: 100%;
+    text-align: center;
+    margin: 10px 0;
+}
+</style>
