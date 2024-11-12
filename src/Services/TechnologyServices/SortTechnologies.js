265112043c4d0ecdbc0137895af9ac9ca5b59c8b
mariogomez441236 <mariogomez441236@gmail.com>
2024-11-11 21:28:33 -0500

    CAMBIO EN EL SERVICIO DE ORDENAR LAS TECNOLOGÍAS. Debido a un camio en el back se actualizó el servicio que ordena las tecnlogías.

--- src/Services/TechnologyServices/SortTechnologies.js
+++ src/Services/TechnologyServices/SortTechnologies.js
@@ -1,13 +1,12 @@
-function SortTechnologies(Technologies) {
-  let SkillsTechnologies = {}
-  Technologies.forEach((Technology) => {
-    let Nombre_Skill = Technology.Skill_Technology.Name_Skill
-    if (!SkillsTechnologies[Nombre_Skill]) {
-      SkillsTechnologies[Nombre_Skill] = []
-    }
-    SkillsTechnologies[Nombre_Skill].push(Technology)
-  })
-  return SkillsTechnologies
+function SortTechnologies(ProgrammingLanguages, Technologies) {
+  let ProgrammingLanguagesTechnologies = []
+  ProgrammingLanguages.forEach((Language) => {
+    const AssociatedTechnologies = Technologies
+      .filter(Technology => Technology.ProgrammingLanguage_Technology.Id_ProgrammingLanguage === Language.Id_ProgrammingLanguage);
+    AssociatedTechnologies.sort((Technology1, Technology2) => Technology1.Name_Technology.localeCompare(Technology2.Name_Technology));
+    ProgrammingLanguagesTechnologies.push({ ProgrammingLanguage: Language, Technologies: AssociatedTechnologies });
+  });
+  return ProgrammingLanguagesTechnologies
 }
 
 export default SortTechnologies
