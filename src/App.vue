761e3c2817e002f040e85764ff5bea68ada61b39
MarioGomez17 <mg441236@gmail.com>
2025-04-20 21:45:43 -0500

    APPLY LAYOUT. The main layout was included in the App.vue and also this component fetch the developer info when the app starts.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <MainLayout>
+    <router-view />
+  </MainLayout>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup lang="ts">
+import MainLayout from './layouts/MainLayout.vue';
+import { onMounted } from 'vue';
+import { useDeveloperStore } from './stores/DeveloperStore';
 
-nav {
-  padding: 30px;
-}
+const DEVELOPER_STORE = useDeveloperStore();
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+onMounted(async () => {
+  await DEVELOPER_STORE.GetDeveloper();
+})
+</script>
