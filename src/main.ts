d05b98d9945ae19cbdfa340a41293d2bbef8aceb
MarioGomez17 <mg441236@gmail.com>
2025-04-20 21:42:48 -0500

    PINIA STORE. The main.ts was changed to use PINIA to manage global stores.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./styles/global.css";
+import { createPinia } from "pinia";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 
-createApp(App).use(router).mount('#app')
+const APP = createApp(App);
+const PINIA = createPinia();
+
+PINIA.use(piniaPluginPersistedState);
+
+APP.use(router);
+APP.use(PINIA);
+APP.mount("#app");
