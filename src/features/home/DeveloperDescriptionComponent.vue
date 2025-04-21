78efded36cb18ed56011486f9c9a1c56d872b39a
MarioGomez17 <mg441236@gmail.com>
2025-04-20 22:08:46 -0500

    HOME COMPONENTS. All the components for the Home View.

---
+++ src/features/home/DeveloperDescriptionComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <p class="Description">{{ PROPS.Description_Developer }}</p>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const PROPS = defineProps({
+    Description_Developer: String
+});
+</script>
+
+<style scoped>
+.Description {
+    color: var(--FourthColor);
+    width: 75%;
+    text-align: left;
+    margin-top: 3rem;
+    font-size: 1.5rem;
+    font-weight: 500;
+    line-height: 2;
+}
+</style>
---
+++ src/features/home/DeveloperNameComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="TitleContainer">
+        <div>
+            <h1 class="MainTitle">{{ PROPS.Name_Developer }} {{ PROPS.LastName_Developer }}</h1>
+            <h2 class="SecondTitle">Web Developer</h2>
+        </div>
+        <IconUserCode class="Icon" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { IconUserCode } from '@tabler/icons-vue';
+
+const PROPS = defineProps({
+    Name_Developer: String,
+    LastName_Developer: String
+});
+
+</script>
+
+<style scoped>
+.TitleContainer {
+    width: 100%;
+    display: flex;
+}
+
+.MainTitle {
+    color: var(--FourthColor);
+    text-align: start;
+    font-size: 6rem;
+}
+
+.SecondTitle {
+    color: var(--ThirdColor);
+    text-align: start;
+    font-size: 3rem;
+}
+
+.Icon {
+    width: auto;
+    height: 12rem;
+    color: var(--FourthColor);
+    filter: drop-shadow(0px 0px 15px);
+}
+</style>
