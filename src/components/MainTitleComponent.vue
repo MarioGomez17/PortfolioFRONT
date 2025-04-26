2980d236f5eee35503a9a9a6d1ac9ad9df718fb0
MarioGomez17 <mg441236@gmail.com>
2025-04-25 22:29:53 -0500

    GLOBAL TITLE COMPONENTS. Main and Second title components were created to use in all views.

---
+++ src/components/MainTitleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="MainTitle">
+        {{ PROPS.Title }}
+    </h1>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const PROPS = defineProps<{
+    Title: string;
+}>();
+</script>
+
+<style scoped>
+.MainTitle {
+    color: var(--FourthColor);
+    text-align: start;
+    font-size: 6rem;
+}
+</style>
---
+++ src/components/SecondTitleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2 class="SecondTitle">
+        {{ PROPS.Title }}
+    </h2>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const PROPS = defineProps<{
+    Title: string;
+}>();
+</script>
+
+<style scoped>
+.SecondTitle {
+    color: var(--ThirdColor);
+    text-align: start;
+    font-size: 3rem;
+    margin: 1rem;
+    margin-left: 0;
+}
+</style>
