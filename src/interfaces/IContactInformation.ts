92ef6c5e8ca44237197865cb10cfa326bf52fc90
MarioGomez17 <mg441236@gmail.com>
2025-05-14 14:01:28 -0500

    ADD NEW INTERFACES New models were add in Back-End so new interfaces were created in Front-End

---
+++ src/interfaces/IContactInformation.ts
@@ -0,0 +1,10 @@
+export interface IContactInformation {
+  Id_ContactInformation: number;
+  PersonName_ContactInformation: string;
+  PersonEmail_ContactInformation: string;
+  PersonPhone_ContactInformation: string;
+  Subject_ContactInformation: string;
+  BudgetRange_ContactInformation: string;
+  Message_ContactInformation: string;
+  ContactMethod_ContactInformation: string;
+}
