7b2019777f18d8bfc0022efea78f5cd65492140f
MarioGomez17 <mg441236@gmail.com>
2025-05-17 13:47:54 -0500

    ADD PERSONAL INFORMATION COMPONENTS Components to show personal information as name, social networks, languages and profile information

---
+++ src/features/about/LanguageComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="LanguageComponent">
+        <h3 class="LanguageName">
+            {{ PROPS.Language_Name }}:
+        </h3>
+        <span class="LanguageLevel">
+            {{ PROPS.Language_Level }}
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const PROPS = defineProps<{
+    Language_Name: string,
+    Language_Level: string,
+}>();
+</script>
+
+<style scoped>
+.LanguageComponent {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+}
+
+.LanguageName {
+    color: var(--SecondColor);
+    font-size: 1.5rem;
+    margin: .5rem 1rem;
+    margin-left: 0;
+}
+
+.LanguageLevel {
+    color: var(--FifthColor);
+    font-size: 1.2rem;
+}
+</style>
---
+++ src/features/about/PersonalInformationComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="PersonalInformationComponent">
+        <SecondTitleComponent Title="Personal Information" />
+        <div class="PersonalInformationContainer">
+            <div class="PersonalInformationSection">
+                <PersonalCharacteristicComponent Characteristic_Title="Full Name"
+                    :Characteristic_Description="PROPS.Name_Developer" :Characteristic_Icon="IconUserCircle"
+                    :Characteristic_IsURL="false" />
+                <PersonalCharacteristicComponent Characteristic_Title="City"
+                    :Characteristic_Description="PROPS.City_Developer" :Characteristic_Icon="IconMapPin"
+                    :Characteristic_IsURL="false" />
+                <PersonalCharacteristicComponent Characteristic_Title="Email"
+                    :Characteristic_Description="PROPS.Email_Developer" :Characteristic_Icon="IconMail"
+                    :Characteristic_IsURL="false" />
+            </div>
+            <div class="PersonalInformationSection">
+                <PersonalCharacteristicComponent Characteristic_Title="LinkedIn"
+                    :Characteristic_Description="PROPS.LinkedIn_Developer"
+                    :Characteristic_Icon="IconBrandLinkedin" :Characteristic_IsURL="true" />
+                <PersonalCharacteristicComponent Characteristic_Title="GitHub"
+                    :Characteristic_Description="PROPS.GitHub_Developer" :Characteristic_Icon="IconBrandGithub"
+                    :Characteristic_IsURL="true" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { IconUserCircle } from '@tabler/icons-vue';
+import { IconMapPin } from '@tabler/icons-vue';
+import { IconMail } from '@tabler/icons-vue';
+import { IconBrandLinkedin } from '@tabler/icons-vue';
+import { IconBrandGithub } from '@tabler/icons-vue';
+import PersonalCharacteristicComponent from './PersonalCharacteristicComponent.vue';
+import SecondTitleComponent from '@/components/SecondTitleComponent.vue';
+
+const PROPS = defineProps<{
+    Name_Developer: string,
+    City_Developer: string,
+    Email_Developer: string,
+    LinkedIn_Developer: string,
+    GitHub_Developer: string,
+}>();
+</script>
+
+<style scoped>
+.PersonalInformationComponent {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+}
+
+.PersonalInformationContainer {
+    display: flex;
+    gap: 3rem;
+}
+</style>
