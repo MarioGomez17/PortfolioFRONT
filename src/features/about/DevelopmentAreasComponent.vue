a905d43659d48ec75cd4ee4794c5f74ae2d478a8
MarioGomez17 <mg441236@gmail.com>
2025-05-17 13:48:45 -0500

    ADD TECHNICAL INFORMATION COMPONENTS Components to show technical  information as technologies, skills, etc.

---
+++ src/features/about/DevelopmentAreasComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="DevelopmentAreasComponent">
+        <SecondTitleComponent Title="Development Areas" />
+        <DevelopmentAreaComponent DevelopmentArea_Title="FrontEnd"
+            :DevelopmentArea_ProgrammingLanguages="PROGRAMMING_LANGUAGE_FRONT"
+            :DevelopmentArea_Technologies="TECHNOLOGIES_FRONT" />
+        <DevelopmentAreaComponent DevelopmentArea_Title="BackEnd"
+            :DevelopmentArea_ProgrammingLanguages="PROGRAMMING_LANGUAGE_BACK"
+            :DevelopmentArea_Technologies="TECHNOLOGIES_BACK" />
+        <DevelopmentAreaComponent DevelopmentArea_Title="Database"
+            :DevelopmentArea_ProgrammingLanguages="PROGRAMMING_LANGUAGE_DATABASE"
+            :DevelopmentArea_Technologies="TECHNOLOGIES_DATABASE" />
+        <DevelopmentAreaComponent DevelopmentArea_Title="Cloud"
+            :DevelopmentArea_ProgrammingLanguages="PROGRAMMING_LANGUAGE_CLOUD"
+            :DevelopmentArea_Technologies="TECHNOLOGIES_CLOUD" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+import SecondTitleComponent from '@/components/SecondTitleComponent.vue';
+import DevelopmentAreaComponent from './DevelopmentAreaComponent.vue';
+import { IProgrammingLanguage } from '@/interfaces/IProgrammingLanguage';
+import { ITechnology } from '@/interfaces/ITechnology';
+
+const TECHNOLOGIES_FRONT = ref<ITechnology[]>([])
+const PROGRAMMING_LANGUAGE_FRONT = ref<IProgrammingLanguage[]>([])
+const TECHNOLOGIES_BACK = ref<ITechnology[]>([])
+const PROGRAMMING_LANGUAGE_BACK = ref<IProgrammingLanguage[]>([])
+const TECHNOLOGIES_DATABASE = ref<ITechnology[]>([])
+const PROGRAMMING_LANGUAGE_DATABASE = ref<IProgrammingLanguage[]>([])
+const TECHNOLOGIES_CLOUD = ref<ITechnology[]>([])
+const PROGRAMMING_LANGUAGE_CLOUD = ref<IProgrammingLanguage[]>([])
+
+const PROPS = defineProps<{
+    DevelopmentArea_ProgrammingLanguages: Array<IProgrammingLanguage>,
+    DevelopmentArea_Technologies: Array<ITechnology>,
+}>();
+
+
+TECHNOLOGIES_FRONT.value = PROPS.DevelopmentArea_Technologies.filter((Technology) => Technology.DevelopmentArea_Technology.Id_DevelopmentArea == 1)
+PROGRAMMING_LANGUAGE_FRONT.value = PROPS.DevelopmentArea_ProgrammingLanguages.filter((ProgrammingLanguage) => ProgrammingLanguage.DevelopmentArea_ProgrammingLanguage.Id_DevelopmentArea == 1)
+TECHNOLOGIES_BACK.value = PROPS.DevelopmentArea_Technologies.filter((Technology) => Technology.DevelopmentArea_Technology.Id_DevelopmentArea == 2)
+PROGRAMMING_LANGUAGE_BACK.value = PROPS.DevelopmentArea_ProgrammingLanguages.filter((ProgrammingLanguage) => ProgrammingLanguage.DevelopmentArea_ProgrammingLanguage.Id_DevelopmentArea == 2)
+TECHNOLOGIES_DATABASE.value = PROPS.DevelopmentArea_Technologies.filter((Technology) => Technology.DevelopmentArea_Technology.Id_DevelopmentArea == 3)
+PROGRAMMING_LANGUAGE_DATABASE.value = PROPS.DevelopmentArea_ProgrammingLanguages.filter((ProgrammingLanguage) => ProgrammingLanguage.DevelopmentArea_ProgrammingLanguage.Id_DevelopmentArea == 3)
+TECHNOLOGIES_CLOUD.value = PROPS.DevelopmentArea_Technologies.filter((Technology) => Technology.DevelopmentArea_Technology.Id_DevelopmentArea == 4)
+PROGRAMMING_LANGUAGE_CLOUD.value = PROPS.DevelopmentArea_ProgrammingLanguages.filter((ProgrammingLanguage) => ProgrammingLanguage.DevelopmentArea_ProgrammingLanguage.Id_DevelopmentArea == 4)
+</script>
+
+<style scoped>
+.DevelopmentAreasComponent {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
---
+++ src/features/about/SkillsComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <SecondTitleComponent Title="Skills" />
+        <div class="SkillsContainer">
+            <TechnicalCharacteristicComponent v-for="Skill in PROPS.Skills" :key="Skill.Id_Skill"
+                :CharacteristicText="Skill.Name_Skill" CharacteristicColor="PurpleCharacteristic" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SecondTitleComponent from '@/components/SecondTitleComponent.vue';
+import { ISkill } from '@/interfaces/ISkill';
+import { defineProps } from 'vue';
+import TechnicalCharacteristicComponent from './TechnicalCharacteristicComponent.vue';
+
+const PROPS = defineProps<{
+    Skills: Array<ISkill>;
+}>();
+</script>
+
+<style scoped>
+.SkillsContainer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1.5rem 1rem;
+}
+</style>
