34275aced0a192546bbf39a827e8cf8b759d5e3a
MarioGomez17 <mg441236@gmail.com>
2025-05-21 15:10:24 -0500

    UPDATE MAIN LAYOUT Main layout change to fit all the components in the UI

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,16 +4,14 @@
         <div class="ContentDiv">
             <main class="MainContent">
                 <slot></slot>
+                <FooterComponent />
             </main>
-            <FooterComponent />
         </div>
     </div>
-    <ContactLinksComponent />
 </template>
 
 <script setup lang="ts">
 import AsideComponent from '@/features/layout/AsideComponent.vue';
-import ContactLinksComponent from '@/features/layout/ContactLinksComponent.vue';
 import FooterComponent from '@/features/layout/FooterComponent.vue';
 </script>
 
@@ -32,13 +30,13 @@ import FooterComponent from '@/features/layout/FooterComponent.vue';
 }
 
 .MainContent {
-    height: 95%;
     width: 100%;
     overflow: auto;
     scrollbar-width: none;
     z-index: 2;
     padding: 1rem;
-    padding-left: 3rem;
+    padding-left: 0;
+    padding-bottom: 0;
     box-sizing: border-box;
 }
 </style>
