470679e54d8db815722b1fd6abf2675a5e2a2d66
MarioGomez17 <mg441236@gmail.com>
2025-05-22 15:45:29 -0500

    ADD EDUCATION LAYOUTS layouts to view the contents of the Education View

---
+++ src/layouts/education/EducationGridLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="EducationGridLayout">
+        <EducationCardComponent v-for="Education in PROPS.Educations_Developer" :key="Education.Id_Education"
+            :Education="Education" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import EducationCardComponent from '@/features/education/EducationCardComponent.vue';
+import { IEducation } from '@/interfaces/IEducation';
+import { defineProps } from 'vue';
+
+const PROPS = defineProps<{
+    Educations_Developer: Array<IEducation>;
+}>();
+</script>
+
+<style scoped>
+.EducationGridLayout {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1.5rem;
+}
+</style>
---
+++ src/layouts/education/EducationListLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="EducationListLayout">
+        <EducationListItemComponent v-for="Education in PROPS.Educations_Developer" :key="Education.Id_Education"
+            :Education="Education" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import EducationListItemComponent from '@/features/education/EducationListItemComponent.vue';
+import { IEducation } from '@/interfaces/IEducation';
+import { defineProps } from 'vue';
+
+const PROPS = defineProps<{
+    Educations_Developer: Array<IEducation>;
+}>();
+</script>
+
+<style scoped>
+.EducationListLayout {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+}
+</style>
